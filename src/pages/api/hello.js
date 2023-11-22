228efa9c430a1926f9f5871d084512f7a7940f03
noahdev0 <96663091+noahdev0@users.noreply.github.com>
2023-11-22 16:39:54 +0100

    hi

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({
+    name: "noahdev0",
+    email: "nouhbenzina@gmail.com",
+    phone: "+213 783 37 05 24",
+    address: "Blida, Algeria",
+    job: "Full Stack Web Developer",
+    github: "noahdev0",
+  });
 }
