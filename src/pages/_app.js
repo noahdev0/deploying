3cd53b5c417ab1aee2e7878ea3e0a1fb5c16ca08
noahdev0 <96663091+noahdev0@users.noreply.github.com>
2023-11-24 10:30:26 +0100

    fixed bug

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,3 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/globals.css";
 import "@/styles/navbar.css";
 import "@/styles/hero.css";
@@ -6,6 +5,7 @@ import "@/styles/features.css";
 import "@/styles/footer.css";
 import "@/styles/partners.css";
 import "@/styles/services.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,3 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "font-awesome/css/font-awesome.min.css";
 import Navbar from "@/components/Navbar";
 import Hero from "../components/Hero";
 import Partners from "@/components/Partners";
@@ -7,6 +5,8 @@ import Services from "@/components/Services";
 import Features from "@/components/Features";
 import Testimonials from "@/components/Testimonials";
 import Footer from "@/components/Footer";
+import "font-awesome/css/font-awesome.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Home() {
   return (
